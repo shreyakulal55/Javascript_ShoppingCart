d8f3e6ada5aa8be53e491b5dd2680ab627310f0d
shreyakulal55 <shreyakulal55@gmail.com>
2023-07-12 22:54:06 +0530

    added

---
+++ JS/Script.js
@@ -0,0 +1,9 @@
+// DOM(Document Object Model)
+// document.body.style.background="red"
+
+// five dom manipulation method:
+// document.getElementById('id')
+// document.getElementsByClassName("class")
+// document.getElementsByTagName("tag")
+// document.querySelector('')
+// document.querySelectorAll('')
